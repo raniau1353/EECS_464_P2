a13c1b4e25e59e7d8308c0bff614cb6b4d3bc7d2
Jacob Trent <50670621+jt-1286@users.noreply.github.com>
2023-11-26 12:53:25 -0500

    Add files via upload

--- Kinematics.py
+++ Kinematics.py
@@ -1,15 +1,40 @@
 import sys
 import csv
 import math
-from FiveBar import FiveBar
+from Linkage import FiveBar
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 class Kinematics:
-    def __init__(self, linkage):
+    # w_x and w_y are front bottom left corner of workspace
+    def __init__(self, linkage, w_x, w_y,):
         self.linkage = linkage
+        self.paper_z = None
+        self.paper_x, self.paper_y = np.meshgrid(np.linspace(w_x, w_x+3.3, num=10), 
+                                                 np.linspace(w_y, w_y+3.3, num=10))
         
+    # COORDINATE SYSTEM: 
+    # origin <0, 0, 0> is j0
+    # +x is towards workspace
+    # +y is left
+    # +z is up
+    
+    # m1, m2: 
+    #   0 deg is +z
+    #   90 deg is +x
+    #   -90 deg is -x
+    
+    # m3:
+    #   0 deg is +x
+    #   90 deg is -y
+    #   -90 deg is +y
+    
+    ###########################
+    ### KINEMATIC FUNCTIONS ###
+    ###########################
+    
+    # returns motor angles in radians
     def inverse(self, END_EFFECTOR_X, END_EFFECTOR_Y, END_EFFECTOR_Z):
         # motor angles
         m1=m2=m3 = None
@@ -18,15 +43,18 @@ class Kinematics:
         m3 = self.linkage.get_angle(END_EFFECTOR_Y, END_EFFECTOR_X, 0., 0.)
         
         # Linkage motors 
-        m1, m2 = self.linkage.get_motor_angles(END_EFFECTOR_X/math.cos(math.radians(m3)), END_EFFECTOR_Z)
+        m1, m2 = self.linkage.get_motor_angles(END_EFFECTOR_X/math.cos(m3), END_EFFECTOR_Z)
         
         print(f"m1: {math.degrees(m1)}\nm2: {math.degrees(m2)}\nm3: {math.degrees(m3)}")
         
         return m1, m2, m3
-        
+    
+    # returns end effector coordinates in linkage space
     def forward(self, MOTOR_ANGLE_1, MOTOR_ANGLE_2, MOTOR_ANGLE_3):
+        # forward extension and z
         r,ee_z = self.linkage.get_ee_coords(MOTOR_ANGLE_1, MOTOR_ANGLE_2)
         
+        # convert forward extention to <x,y>
         ee_x = math.cos(MOTOR_ANGLE_3)*r
         ee_y = math.sin(MOTOR_ANGLE_3)*r
         
@@ -34,13 +62,23 @@ class Kinematics:
         
         return ee_x, ee_y, ee_z
     
+    #########################
+    ### CONTROL FUNCTIONS ###
+    #########################
+    
     def calibrate(self):
+        # read in list of calibration points
         points_list = []
         with open('calibration_data.csv') as cal_file:
             cal_angles = csv.reader(cal_file, delimiter=' ')
             for row in cal_angles:
+                # angles read from file
                 m1, m2, m3 = row
-                x, y, z = self.forward(float(m1), float(m2), float(m3))
+                
+                # convert motor angles to ee point
+                x, y, z = self.forward(math.radians(float(m1)), 
+                                       math.radians(float(m2)), 
+                                       math.radians(float(m3)))
                 points_list.append([x, y, z])
         points = np.array(points_list)
         
@@ -49,32 +87,61 @@ class Kinematics:
         A = np.c_[points[:, 0], points[:, 1], np.ones(len(points[:, 0]))]
         
         # plane coefficients Z = c0*X + c1*Y + c2
-        C,_,_,_ = np.linalg.lstsq(A, b)
+        C,_,_,_ = np.linalg.lstsq(A, b, rcond=None)
+        self.paper_z = self.paper_x*C[0] + self.paper_y*C[1] + C[2]
         
-        return points, C
+        return points
 
-
+      # takes in start coordinate of square in paper frame and side length, draws a square
+    def draw(self, p_x, p_y, length):
+        # MOVE TO START
+        ee_x, ee_y, ee_z = self.__paper_to_linkage(p_x, p_y)
+        self.__move_linkage(ee_x, ee_y, ee_z)
+        
+        # DRAW BOTTOM LINE
+        ee_x, ee_y, ee_z = self.__paper_to_linkage(p_x+length, p_y)
+        self.__move_linkage(ee_x, ee_y, ee_z)
+        
+        # DRAW RIGHT LINE
+        ee_x, ee_y, ee_z = self.__paper_to_linkage(p_x+length, p_y+length)
+        self.__move_linkage(ee_x, ee_y, ee_z)
+        
+        # DRAW TOP LINE
+        ee_x, ee_y, ee_z = self.__paper_to_linkage(p_x, p_y+length)
+        self.__move_linkage(ee_x, ee_y, ee_z)
+        
+        # DRAW LEFT LINE
+        ee_x, ee_y, ee_z = self.__paper_to_linkage(p_x, p_y)
+        self.__move_linkage(ee_x, ee_y, ee_z)
+        
+    # convert point in paper reference frame to linkage reference frame
+    def __paper_to_linkage(self, p_x, p_y):
+        # TODO
+        assert(False)
+        
+    # move linkage to designated end effector coordinates
+    def __move_linkage(self, ee_x, ee_y, ee_z):
+        m1, m2, m3 = self.inverse(ee_x, ee_y, ee_z)
+        # TODO
+        
+        
 ## DEBUG SCRIPT ##      
 if __name__ == "__main__":
-    linkage = FiveBar(45, 35, 30, 35, 40, 70, 20)
-    model = Kinematics(linkage)
+    linkage = FiveBar(3, 6, 5, 5, 4, 5, 2.5)
+    model = Kinematics(linkage, 15, -1.65)
     
-    raw, fit = model.calibrate()
+    #model.inverse(4.45, 0, 3.21-2.5)
     
+    
+    raw = model.calibrate()
+  
     # plot raw data
     plt.figure()
     ax = plt.subplot(111, projection='3d')
-    ax.scatter(raw, color='b')
-    
-    # plane
-    X, Y = np.meshgrid(np.arange(ax.get_xlim[0], ax.get_xlim[1]), 
-                       np.arange(ax.get_ylim[0], ax.get_ylim[1]))
-    
-    Z = X*fit[0] + Y*fit[1] + fit[2]
-    
+    ax.scatter(raw[:,0], raw[:,1], color='b')    
     
     # plot plane
-    ax.plot_wireframe(X,Y,Z, color='k')
+    ax.plot_wireframe(model.paper_x,model.paper_y,model.paper_z, color='k')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
