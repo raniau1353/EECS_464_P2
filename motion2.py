723057cead6ed0856ae5f20e55e2a41cc55bd4d5
Ziyu Guo <ziyuguo@umich.edu>
2023-11-28 21:19:13 -0500

    added pose recorder tests

--- motion2.py
+++ motion2.py
@@ -6,7 +6,6 @@ import math
 import numpy as np
 import sys
 from time import sleep
-from os.path import exists
 
 from joy.decl import *
 from joy import JoyApp
@@ -15,10 +14,9 @@ from joy.misc import loadCSV
 
 import ckbot.logical as L
 
-# global variable to specify how many steps to take between each point in the square
-STEPS = 30
 
-def test_kinematics(m1, m2, m3):
+def test_kinematics():
+    print('testing kinematics')
     #                 l1  l2  l3  l4  lg  le  lift
     linkage = FiveBar(30, 60, 50, 50, 40, 51.5, 28.5) # cm
     model = Kinematics(linkage)
@@ -29,15 +27,15 @@ def test_kinematics(m1, m2, m3):
     # m2: Nx2D_
     # m3: Nx2E
 
-    m1, m2, m3 = model.inverse(float(m1), float(m2), float(m3))
-    # print("INVERSE")
-    # print(f"m1: {math.degrees(m1)}\nm2: {math.degrees(m2)}\nm3: {math.degrees(m3)}")
+    m1, m2, m3 = model.inverse(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
+    print("INVERSE")
+    print(f"m1: {math.degrees(m1)}\nm2: {math.degrees(m2)}\nm3: {math.degrees(m3)}")
     
     m1_f = ((math.degrees(m1) - 45)*(-1))*100
     m2_f = (math.degrees(m2)*(-1))*100
     m3_f = math.degrees(m3)*100
-    # print("CORRECTED:")
-    # print(f"m1: {m1_f}\nm2: {m2_f}\nm3: {m3_f}")
+    print("CORRECTED:")
+    print(f"m1: {m1_f}\nm2: {m2_f}\nm3: {m3_f}")
 
     m1_i = c.at.Nx2F.get_pos()
     m2_i = c.at.Nx2D.get_pos()
@@ -46,84 +44,71 @@ def test_kinematics(m1, m2, m3):
     m1_ls = np.linspace(m1_i, m1_f, num=25)
     m2_ls = np.linspace(m2_i, m2_f, num=25)
     m3_ls = np.linspace(m3_i, m3_f, num=25)
+    
+    print(f"m1_ls: {m1_ls}")
+    print(f"m2_ls: {m2_ls}")
+    print(f"m3_ls: {m3_ls}")
 
     for i in range(len(m1_ls)):
         c.at.Nx2F.set_pos(m1_ls[i])
         c.at.Nx2D.set_pos(m2_ls[i])
         c.at.Nx2E.set_pos(m3_ls[i])
-        # print(f"Moving to {m1_ls[i]}, {m2_ls[i]}, {m3_ls[i]}")
         sleep(0.1)
+    
+# def square(p1, p2, p3, p4)
+#     test_kinematics(p1[0], p1[1], p1[3])
+#     sleep(2)
+#     test_kinematics(p2[0], p2[1], p2[3])
+#     sleep(2)
+#     test_kinematics(p3[0], p3[1], p3[3])
+#     sleep(2)
+#     test_kinematics(p4[0], p4[1], p4[3])
+#     sleep(2)
+#     test_kinematics(p1[0], p1[1], p1[3])
+#     sleep(2)
+
+# def pose_recorder()
+    
+#     while(true):
+#         if(keyboard.event)
+
+
+# def on_key_w():
+#     #                 l1  l2  l3  l4  lg  le  lift
+#     linkage = FiveBar(30, 60, 50, 50, 40, 51.5, 28.5) # cm
+#     model = Kinematics(linkage)
+
+#     c = L.Cluster(count = 3)
+
+#     if e.event_type == keyboard.W:
+#         m1_i = c.at.Nx2F.get_pos()
+#         m2_i = c.at.Nx2D.get_pos()
+#         m3_i = c.at.Nx2E.get_pos()
+#         x, y, z = model.forward(m1_i, m2_i, m3_i)
+#         test_kinematics(x - 1, y, z)
     
 def create_csv(filename):
     # create a csv file to write pose positions to 
-    with open(filename, mode='w', newline='') as csv_file:
-        print(f"CSV file '{filename}' created successfully.")
+    print(filename)
 
 def pose(filename):
-    c = L.Cluster(count = 3)
-    # get current motor positions
-    m1 = c.at.Nx2F.get_pos() # UNCOMMENT
-    m2 = c.at.Nx2D.get_pos() # UNCOMMENT
-    m3 = c.at.Nx2E.get_pos() # UNCOMMENT
-
-    # m1, m2, m3 = 10, 11, 12 # COMMENT
-    current_pos = [m1, m2, m3]
-    
-    # check if csv file exists
-    if exists(filename):
-        # write each motor position as a row to the current csv file
-        with open(filename, mode='a', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(current_pos)
-            print(f"Wrote positions {current_pos} to '{filename}' successfully.")
-    else:
-        print(f"Error: The CSV file '{filename}' does not exist. Run 'start <filename>' from the command line to create a new csv file")
-
+    # write each motor position as a row to the current csv file
+    print(filename)
 
 def run(filename):
-    c = L.Cluster(count = 3)
-    # check if csv file exists
-    if exists(filename):
-        # loop through each row of the csv file and command the motors to that position
-        with open(filename, mode='r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            first_row = next(csv_reader, None)
-
-            # go to starting position
-            m1_i, m2_i, m3_i = map(float, first_row)
-            print(f"Moving to starting position {first_row}")
-            c.at.Nx2F.set_pos(m1_i)
-            c.at.Nx2D.set_pos(m2_i)
-            c.at.Nx2E.set_pos(m3_i)
-
-            for row in csv_reader:
-                # ex: ['10', '11', '12']
-                print(f"Moving to position {row}")
-                m1_curr, m2_curr, m3_curr = map(float, row)
-
-                m1_ls = np.linspace(m1_i, m1_curr, num=STEPS)
-                m2_ls = np.linspace(m2_i, m2_curr, num=STEPS)
-                m3_ls = np.linspace(m3_i, m3_curr, num=STEPS)
-
-                # move arm
-                for i in range(len(m1_ls)):
-                    # print(f"Moving to {m1_ls[i]}, {m2_ls[i]}, {m3_ls[i]}")
-                    c.at.Nx2F.set_pos(float(m1_ls[i]))
-                    c.at.Nx2D.set_pos(float(m2_ls[i]))
-                    c.at.Nx2E.set_pos(float(m3_ls[i]))
-                    sleep(0.5)
-                
-                m1_i, m2_i, m3_i = m1_curr, m2_curr, m3_curr
-
-    else:
-        print(f"Error: The CSV file '{filename}' does not exist. Run 'start <filename>' from the command line to create a new csv file")
+    # loop through each row of the csv file and command the motors to that position
+    # sleep between each position
+    print(filename)
 
 def cal_3p():
     #                 l1  l2  l3  l4  lg  le  lift
     linkage = FiveBar(30, 60, 50, 50, 40, 55, 28.5) # cm
     model = Kinematics(linkage)
 
+
+
     M = model.quick_cal()
+    
     
     # Define coordst
     # paper: 21.5cm x 27.9cm
@@ -131,6 +116,7 @@ def cal_3p():
     p1 = [7.5, 17.5]
     p2 = [17.5, 17.5]
     p3 = [17.5, 7.5]
+
 
     # CSV creation 
     with open("output.csv", 'w') as csv_out:
@@ -169,27 +155,13 @@ def cal_3p():
 
 def main():
     if len(sys.argv) > 1:
-        # ex: motion.py 10 16 0
         if len(sys.argv) == 4:
-            x = float(sys.argv[1])
-            y = float(sys.argv[2])
-            z = float(sys.argv[3])
-            if (y > 20 or y < -20):
-                print('Invalid y-range. Y-range is from [-20, 20]')
-            elif (x < 30 or x > 70):
-                print('Invalid x-range. X-range is from [30, 70]')
-            elif (z < -25 or z > 35):
-                print('Invalid z-range. Z-range is from [-20, 35]')
-            else:
-                test_kinematics(x, y, z)
+            test_kinematics()
         elif len(sys.argv) == 3:
-            # ex: motion.py start square.csv
             if sys.argv[1] == 'start':
                 create_csv(sys.argv[2])
-            # ex: motion.py pose square.csv
             elif sys.argv[1] == 'pose':
                 pose(sys.argv[2])
-            # ex: motion.py run square.csv
             elif sys.argv[1] == 'run':
                 run(sys.argv[2])
         else:
@@ -197,4 +169,5 @@ def main():
             return
 
 if __name__ == "__main__":
+    #test_kinematics()
     main()
